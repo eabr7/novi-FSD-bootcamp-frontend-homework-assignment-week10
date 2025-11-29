9f327755cdf31f72036e1ddf4d69fd912abb939c
eabr7 <e.bruins@protonmail.com>
2025-11-29 20:53:23 +0100

    week 10, assignments made until 2c.

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,18 @@ import tvSizes from "./helpers/tvSizes.js";
 import tvImage from './assets/tvImage.jpeg';
 import checkIcon from './assets/check.png';
 import minusIcon from './assets/minus.png';
+import showOefeningOneInConsole from './constants/oefenbestand.js';
 
 function App() {
-  return (
+    showOefeningOneInConsole();
+
+    return (
       <>
           <main className="outer-container">
               <section className="inventory-container section">
                  <h1>Tech It Easy Dashboard</h1>
                  <h2>Verkoop overzicht</h2>
-                 <div className="all-tvs">
+                 <div className="all-tvs-dashboard">
                      <article className="sold-tvs">
                          <p>Aantal verkochte producten</p>
                          <p>{totalSold(inventory)}</p>
@@ -64,6 +67,41 @@ function App() {
                       <button type="button" className="cheapest-button" onClick={() => console.log("Goedkoopste eerst")}>Goedkoopste eerst</button>
                       <button type="button" className="sport-button" onClick={() => console.log("Meest geschikt voor sport eerst")}>Meest geschikt voor sport eerst</button>
                   </div>
+              </section>
+              <section className="all-tv-brands section">
+                  <h2>Alle TV merken</h2>
+                  <ul>
+                      {inventory.map((tv) => {
+                          return <li key={tv.type}>{tv.brand}</li>
+                          }
+                      )}
+                  </ul>
+              </section>
+              <section className="all-tvs section">
+                  {inventory.map((tv) => {
+                      return <article className="best-tv" key={tv.type}>
+                      <span className="tv-image-box">
+                          <img src={tvImage} alt="Best stelling TV"/>
+                      </span>
+                          <div className="best-tv-info">
+                              <p>{tvName(tv)}</p>
+                              <p className="best-tv-price">{tvPrice(tv)}</p>
+                              <p>{tvSizes(tv)}</p>
+                              <div className="options-row">
+                                  <img className="icons" src={checkIcon} alt="Check Icon"/>
+                                  <p>Wifi</p>
+                                  <img className="icons" src={minusIcon} alt="Check Icon"/>
+                                  <p>Speech</p>
+                                  <img className="icons" src={checkIcon} alt="Check Icon"/>
+                                  <p>HDR</p>
+                                  <img className="icons" src={checkIcon} alt="Check Icon"/>
+                                  <p>Bluetooth</p>
+                                  <img className="icons" src={minusIcon} alt="Check Icon"/>
+                                  <p>Ambilight</p>
+                              </div>
+                          </div>
+                      </article>
+                  })}
               </section>
           </main>
       </>
